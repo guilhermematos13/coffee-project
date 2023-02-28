f9d7abae6d72af9e5fb09a4a685ca53afa3145b0
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-02-28 14:51:54 -0300

    feat: create routes and config styled component

---
+++ src/Router.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import { Checkout } from "./pages/Checkout";
+import { Home } from "./pages/Home";
+import { DefaultLayout } from "./pages/layouts/DefaultLayout";
+import { Success } from "./pages/Success";
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/success" element={<Success />} />
+      </Route>
+    </Routes>
+  );
+}
