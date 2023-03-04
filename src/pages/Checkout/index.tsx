56722d0289697660274059b39204b0c000b2b75d
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-03-04 13:10:12 -0300

    feat:  create payment values details and button for confirmed order

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -6,9 +6,13 @@ import { PaymentDetails } from "./components/PaymentDetails";
 import {
   CheckoutContainer,
   CoffeeSelectContainer,
+  ConfirmedOrderButton,
   LineTest,
   OrderDetailsContainer,
+  PaymentDataContainer,
   TitleForm,
+  ValueDetailsTitle,
+  ValuesDetailsPrice,
 } from "./styles";
 
 export function Checkout() {
@@ -29,6 +33,22 @@ export function Checkout() {
           />
           <LineTest></LineTest>
           <CoffeeItem image={Latte} price="R$ 19,90" title="Latte" />
+          <LineTest></LineTest>
+          <PaymentDataContainer>
+            <div className="linePayment">
+              <ValueDetailsTitle>Total de itens</ValueDetailsTitle>
+              <ValuesDetailsPrice>R$ 29,70</ValuesDetailsPrice>
+            </div>
+            <div className="linePayment">
+              <ValueDetailsTitle>Entrega</ValueDetailsTitle>
+              <ValuesDetailsPrice>R$ 3,50</ValuesDetailsPrice>
+            </div>
+            <div className="linePayment">
+              <strong className="">Total</strong>
+              <strong>R$ 33,20</strong>
+            </div>
+          </PaymentDataContainer>
+          <ConfirmedOrderButton>Confirma Pedido</ConfirmedOrderButton>
         </CoffeeSelectContainer>
       </div>
     </CheckoutContainer>
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -36,3 +36,43 @@ font-weight: 700;
 font-size: 1.125rem;
 margin-bottom: 1rem;
 `
+
+export const PaymentDataContainer = styled.div`
+display: flex;
+flex-direction: column;
+gap:0.75rem;
+width: 100%;
+
+.linePayment{
+    display: flex;
+    align-items: center;
+    width: 100%;
+    justify-content: space-between;
+}
+
+strong {
+    font-size: 1.25rem;
+    color: ${props => props.theme.baseSubTitle}
+}`
+
+export const ValueDetailsTitle = styled.p`
+font-size: 0.75rem;
+color: ${props => props.theme.baseText};`
+
+export const ValuesDetailsPrice = styled.p`
+font-size: 1rem;
+color: ${props => props.theme.baseText};`
+
+export const ConfirmedOrderButton = styled.button`
+border: 0;
+padding: 0.75rem 0.5rem;
+margin-top: 1.5rem;
+font-size: 0.875rem;
+font-weight: bold;
+text-transform: uppercase;
+color: ${props => props.theme.white};
+background: ${props => props.theme.yellow};
+border-radius: 6px;
+cursor: pointer;
+
+`
