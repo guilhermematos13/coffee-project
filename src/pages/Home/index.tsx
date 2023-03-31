597f9d43431734e3127987be2f768cf99419e062
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-03-31 15:44:38 -0300

    feat: create success page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,11 @@
 import { CoffeeContent } from "./components/CoffeeContent";
 import { Summary } from "./components/Summary";
-import { HomeContainer } from "./styles";
 
 export function Home() {
   return (
-    <HomeContainer>
+    <div>
       <Summary />
       <CoffeeContent />
-    </HomeContainer>
+    </div>
   );
 }
---
+++ src/pages/Success/styles.ts
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+width: 100%;
+padding: 2rem 10rem;
+display: flex;
+flex-direction: column;
+
+margin-top: 5rem;
+
+h1 {
+    color: ${props => props.theme.yellowDark};
+    font-family: 'Baloo 2', sans-serif;
+    margin-bottom: 0.25rem;
+}
+
+p {
+    font-size: 1.25rem;
+    font-family: 'Roboto', sans-serif;
+}
+`
+
+export const ContentContainer = styled.div`
+display: flex;
+justify-content: space-between;
+gap: 6.5rem;
+margin-top: 2.5rem;
+`
+
+export const UserData = styled.div`
+
+width: 50%;
+display: flex;
+justify-content: center;
+flex-direction: column;
+padding: 2.5rem;
+background: linear-gradient(white, white) padding-box, linear-gradient(to right bottom, #DBAC2C, #8047F8) border-box;
+border-radius: 6px 36px 6px 36px;
+border: 1px solid transparent;
+
+div {
+    display: flex;
+    margin-bottom: 2rem;
+}
+
+p {
+    font-size: 0.99rem;
+}
+`
+
+export const FirstIcon = styled.div`
+margin-top: 1rem`
+
+interface IconStyledProps {
+    variant: 'purple' | 'yellow' | 'yellowDark'
+}
+
+
+export const IconStyled = styled.span<IconStyledProps>`
+display: flex;
+align-items: center;
+justify-content: center;
+height: 2rem;
+width: 2rem;
+margin-right: 8px;
+border-radius: 999px;
+background-color: ${props => props.variant === 'purple' ? props.theme.purple : props.variant === 'yellow' ? props.theme.yellow : props.variant === 'yellowDark' && props.theme.yellowDark};
+color: ${props => props.theme.white}
+
+`
